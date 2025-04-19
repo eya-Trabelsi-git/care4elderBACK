8a5839a7dab037a3decb0429ae06c9fbdcd9006a
ghassendallagi <ghassen.dallegi.8@gmail.com>
2025-04-20 00:30:14 +0100

    services et dossier

--- factureService/src/main/java/com/care4elder/factureservice/repositories/FactureRepository.java
+++
@@ -1,7 +0,0 @@
-package com.care4elder.factureservice.repositories;
-
-import com.care4elder.factureservice.entities.Facture;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FactureRepository extends MongoRepository<Facture, String> {
-}
