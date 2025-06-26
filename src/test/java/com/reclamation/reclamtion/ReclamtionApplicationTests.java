f51d5b03d641065bbf41dfb52f0436aeb460c047
sabrinealoui <Sabrine.ALOUI@esprit.tn>
2025-06-26 16:33:41 +0100

    quatréme

--- src/test/java/com/reclamation/reclamtion/ReclamtionApplicationTests.java
+++ src/test/java/com/reclamation/reclamtion/ReclamtionApplicationTests.java
@@ -2,12 +2,14 @@ package com.reclamation.reclamtion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // tells Spring to use application-test.yml
 class ReclamtionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // This should pass now that the context is correctly loading
+    }
 }
